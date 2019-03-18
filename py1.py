98934f99e3a01f529fdb8e7d11f3c4ed6a72bf23
2314372037 <zh2314372037@outlook.com>
2019-03-18 13:01:39 +0800

    优化代码，速度更快

--- py1.py
+++ py1.py
@@ -49,7 +49,7 @@ class netbian():
     def getImageUrl1(self,page1):
         urlList=r"/desk/[0-9]*-{1,}[0-9]*x[0-9]*.htm"
         imgUrl=re.findall(urlList,page1)
-        return imgUrl
+        return imgUrl[0]#返回第一张
 
     def getImageUrl2(self,page2):
         urlList=r"http://\w*.\w*.\w*./file/[0-9]*/[0-9]*\w*.\w*.jpg"
@@ -65,38 +65,36 @@ class netbian():
             imgUrl = self.getImageUrl(page)  # 获取第一个页面的第cou个URL
 
             for i in range(len(imgUrl)):
-                page1 = self.getHtml(self.netbianHostUrl + imgUrl[i])# 获取第i个页面
-                imgUrl1 = self.getImageUrl1(page1)# 获取第二个页面的第一个URL
+                page1 = self.getHtml(self.netbianHostUrl + imgUrl[i])# 获取第二个页面
+                imgUrl1 = self.getImageUrl1(page1)# 获取第二个页面的第i个URL
 
-                for j in range(len(imgUrl1)):
-                    page2 = self.getHtml(self.netbianHostUrl + imgUrl1[j]) # 获取第j个页面
-                    imgUrl2 = self.getImageUrl2(page2)# 获取第三个页面的第一个URL
-                    imgUrl2rr2 = self.removeRepeat(imgUrl2) # 去重
+                page2 = self.getHtml(self.netbianHostUrl + imgUrl1) # 获取第三个页面-已经获取到图片最终地址
+                imgUrl2 = self.getImageUrl2(page2)# 获取图片URL地址 例:http://img.netbian.com/file/2018/1113/fef678dcdd5f6a6c894363daab498f4c.jpg
 
-                    print("当前图片:" + str(imgUrl2rr2))
+                imgUrl2rr2 = self.removeRepeat(imgUrl2) # 去重+排除指定图片
 
-                    if len(imgUrl2rr2)==0:
-                        print("当前集合没有数据,取消保存")
+                if len(imgUrl2rr2)==0:
+                    print("当前图片不存在,跳过")
+                    continue
+
+                time.sleep(self.intervalTime)
+
+                if not os.path.exists(os.getcwd()+"\\image"):#当前路径是否存在image文件夹
+                    os.mkdir(os.getcwd()+"\\image")
+
+                try:
+                    imagePathAndName=os.getcwd()+"\\image\\" + self.imgName + str(cou)+str(i) + ".jpg"#即将保存的图片
+                    if os.path.exists(imagePathAndName):
+                        print("当前图片已存在，跳过！")
                         continue
-
-                    time.sleep(self.intervalTime)
-
-                    if not os.path.exists(os.getcwd()+"\\image"):#当前路径是否存在image文件夹
-                        os.mkdir(os.getcwd()+"\\image")
-
-                    try:
-                        imagePathAndName=os.getcwd()+"\\image\\" + self.imgName + str(cou)+str(i) + ".jpg"#即将保存的图片
-                        if os.path.exists(imagePathAndName):
-                            print("当前图片已存在，跳过！")
-                            continue
                         
-                        urllib.request.urlretrieve(imgUrl2rr2[0],imagePathAndName)
-                        print("第"+str(cou+1)+"页,第"+str(i+1)+"张图片保存完成！")
-                        imageSaveCount=imageSaveCount+1
-                    except Exception as e:
-                        print("第"+str(cou+1)+"页,第"+str(i+1)+"张图片保存失败！\r\n异常信息:" + str(e))
+                    urllib.request.urlretrieve(imgUrl2rr2[0],imagePathAndName)
+                    print("第"+str(cou+1)+"页,第"+str(i+1)+"张图片保存完成！")
+                    imageSaveCount=imageSaveCount+1
+                except Exception as e:
+                    print("第"+str(cou+1)+"页,第"+str(i+1)+"张图片保存失败！\r\n异常信息:" + str(e))
         print("ヾﾉ≧∀≦)o 共保存了"+str(imageSaveCount)+"张图片！")
 
 if __name__ == "__main__":
-    bian=netbian("美女",10)#参数说明(要搜索的图片名，页数)
+    bian=netbian("猫",10)#参数说明(要搜索的图片名，页数)
     bian.start()
